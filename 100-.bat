a07033d16a209d4c7f09ad6c4d3fc6ad5f3eef80
wufake <wufake70@gmail.com>
2023-10-08 18:59:34 +0800

    8/10/2023 18:59

--- 100-.bat
+++
@@ -1,7 +0,0 @@
-runas /user:administrator "cmd /c %1"
-@echo off
-echo wifi devices:
-netsh wlan show profiles | findstr :
-set /p a=please enter wifi name:
-netsh wlan show profiles "%a%" key=clear | findstr Key
-pause
